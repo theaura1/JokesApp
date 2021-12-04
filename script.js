830b560b95b7e850f4fa9e1f4a8ef059fb3a0ddc
theaura1 <43887026+theaura1@users.noreply.github.com>
2021-12-04 06:12:54 +0530

    Update script.js
    used async/await this time

--- script.js
+++ script.js
@@ -5,24 +5,44 @@
 }
 
 //using promises
+// const jokes = document.querySelector(".joke");
+// const jokebtn = document.querySelector(".jokebtn");
+// const generatejokes = () => {
+//   const setHeader = {
+//     object, headers is a key value
+//     headers: {
+//       Accept: "application/json", //value is another key-value
+//     },
+//   };
+//res is a variable, we were getting result of fetch in html, so in order to get it in json, we converted it as res.json
+//   fetch("https://icanhazdadjoke.com/", setHeader)
+//     .then((res) => res.json())
+//     .then((data) => {
+//       jokes.innerHTML = data.joke;
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//     });
+// };
+// jokebtn.addEventListener("click", generatejokes);
+// generatejokes();
+
+//!using async/await
 const jokes = document.querySelector(".joke");
 const jokebtn = document.querySelector(".jokebtn");
-const generatejokes = () => {
-  const setHeader = {
-    //object, headers is a key value
-    headers: {
-      Accept: "application/json", //value is another key-value
-    },
-  };
-  //res is a variable, we were getting result of fetch in html, so in order to get it in json, we converted it as res.json
-  fetch("https://icanhazdadjoke.com/", setHeader)
-    .then((res) => res.json())
-    .then((data) => {
-      jokes.innerHTML = data.joke;
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+const generatejokes = async () => {
+  try {
+    const setHeader = {
+      headers: {
+        Accept: "application/json",
+      },
+    };
+    const res = await fetch("https://icanhazdadjoke.com/", setHeader);
+    const data = await res.json();
+    jokes.innerHTML = data.joke;
+  } catch (error) {
+    console.log(`${error}`);
+  }
 };
 jokebtn.addEventListener("click", generatejokes);
 generatejokes();
