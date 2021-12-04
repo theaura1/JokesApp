10210d183edf263cc38f89939ea01ff69facd8e0
theaura1 <43887026+theaura1@users.noreply.github.com>
2021-12-04 05:55:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+{
+  /* <div class="card-text joke">Loading..</div>
+ <a href="#" class="btn btn-primary jokebtn">Browse Next</a>
+                    */
+}
+
+//using promises
+const jokes = document.querySelector(".joke");
+const jokebtn = document.querySelector(".jokebtn");
+const generatejokes = () => {
+  const setHeader = {
+    //object, headers is a key value
+    headers: {
+      Accept: "application/json", //value is another key-value
+    },
+  };
+  //res is a variable, we were getting result of fetch in html, so in order to get it in json, we converted it as res.json
+  fetch("https://icanhazdadjoke.com/", setHeader)
+    .then((res) => res.json())
+    .then((data) => {
+      jokes.innerHTML = data.joke;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+jokebtn.addEventListener("click", generatejokes);
+generatejokes();
